d8427c816ac4d424060e544e5d613a685ec475df
charan214 <122966997+charan214@users.noreply.github.com>
2023-10-11 22:34:16 +0530

    Add files via upload

---
+++ Nuatural-num_Leap_Fact.py
@@ -0,0 +1,26 @@
+'''i=0
+while(i<=10):
+    print("number:",i)
+    i=i+1'''
+#sum of natural numbers and avg#
+'''sum=0
+n=int(input("Enter count:"))
+while(n>0):
+    sum=sum+n
+    n=n-1
+avg=sum/2
+print(f"average of sum of {n} natural numbers is {avg}")'''
+#leap year#
+'''year=int(input("Enter year:"))
+if(year%100==0 and year%400==0 and year%4==0):
+    print(f'{year} is leapyear')
+else:
+    print(f'{year} is not a leapyear')'''
+#factorial
+'''mul=1
+i=5
+while(i>j):
+    mul=mul*i
+    i=i-1
+print(f'{i} factorial {mul}')'''
+    
